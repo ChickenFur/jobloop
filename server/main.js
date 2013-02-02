13fb42ca44349ae285cdf6c590c3d2b7dd49d771
Sherah <sherah@sherah.org>
2013-02-02 10:09:42 -0800

    tweaking test data

--- server/main.js
+++ server/main.js
@@ -7,10 +7,12 @@ Meteor.methods({
     console.log("Getting Company Names")
   },
   fillDBWithTestData : function(userId) {
+    console.log("fillDBWithTestData was called.");
     TestFunctions.insertTestData(this.userId)
     console.log(JobLoopUsers.find().fetch()[0])
   },
   deleteDB : function(){
+    console.log("deleteDB was called.");
     JobLoopUsers.remove({});
     console.log("Number of users: ",JobLoopUsers.find().count())
   }
