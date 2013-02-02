f97286517e6eeb928fc05bd72c967d56a497c3c5
Sherah <sherah@sherah.org>
2013-02-01 21:31:54 -0800

    replaying changes

--- server/main.js
+++ server/main.js
@@ -1,8 +1,7 @@
 JobLoopUsers = new Meteor.Collection("job-loop-users")
 
 Meteor.publish("user-data", function(){
-  JobLoopUsers.findOne({meteorUserId : this.userId}
-	)
+  JobLoopUsers.findOne({meteorUserId : this.userId})
 })
 
 Meteor.methods({
