864453113841474045aa827db185d6707848fe35
ChickenFur <chickenfur@gmail.com>
2013-02-02 17:15:05 -0800

    Edit and Save button Working

---
+++ client/companytab/company.js
@@ -0,0 +1,41 @@
+DnB = {
+  get_data_async : function (query, callback){
+    results = Meteor.call("getCompanyData", query)
+
+  }
+}
+
+Template.company.events = ({
+  "click .editCompany" : function (event, template){
+   
+    currentName = template.find('.companyNameLabel')
+    currentNameVal = currentName.textContent;
+    inputTag = "<input class='companyNameInput' value='"+  
+                    currentNameVal + "'></input>"
+    parent = currentName.parentElement;
+    $(currentName).addClass("hiddenItem");
+    $(parent).append(inputTag);
+    editButton = template.find('.editCompany');
+    $(editButton).addClass("hiddenItem");
+    saveButton = template.find('.saveCompany');
+    $(saveButton).removeClass("hiddenItem");
+    $(template.find('.companyNameInput') ).typeahead({source : DnB.get_data_async})
+
+  },
+  "click .saveCompany" : function (event, template){
+    saveButton = template.find('.saveCompany');
+    $(saveButton).addClass("hiddenItem");
+
+    editButton = template.find('.editCompany');
+    $(editButton).removeClass("hiddenItem");
+
+    currentName = template.find('.companyNameLabel')
+    $(currentName).contentText = $(template.find('.companyNameInput')).val()
+    $(currentName).removeClass("hiddenItem");
+    $(template.find('.companyNameInput')).remove();
+
+  }
+
+
+
+})
--- server/main.js
+++ server/main.js
@@ -19,5 +19,23 @@ Meteor.methods({
     console.log("deleteDB was called.");
     JobLoopUsers.remove({});
     console.log("Number of users: ",JobLoopUsers.find().count())
-  }
+  },
+  getCompanyNames : function(name){
+        console.log("Getting Company Data");
+        var url = "http://dnbdirect-sandbox.dnb.com/DnBAPI-13/rest/search/company/"+company;
+        var header ={
+          'Accept' : 'application/json',
+          'Content-type' : 'application/json',
+          'API-KEY' : '5p83tw3zsmupe48wdgq5dz8a',
+          'username' : 'sandboxAPI',
+          'password' : 'DnBD1rect'
+          }
+        var options={
+          headers: header,
+          dataType: "jsonp"
+        }
+        results = Meteor.http.get(url, options).content
+        console.log("Results: ", results );
+        return results;
+      }   
 });
